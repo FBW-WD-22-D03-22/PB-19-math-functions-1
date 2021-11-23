f65b375bbbcff99376e38e4c8f9accd9d82104ff
OrhanKadirov <orhan.kadirov.gs@gmail.com>
2021-11-23 11:57:20 +0100

    Orhan's lösung

---
+++ math_functions.js
@@ -0,0 +1,24 @@
+const a = 5;
+const b = 6;
+const c = 7;
+
+function addition() {
+    console.log("Aufgabe 1) let res  =", a + b + c)
+}
+
+function subtraktion() {
+    console.log("Aufgabe 2) let res  =", a - b - c)
+}
+
+function multiplikation() {
+    console.log("Aufgabe 3) let res  =", a * b * c)
+}
+
+function division() {
+    console.log("Aufgabe 4) let res  =", a / b / c)
+}
+
+addition();
+subtraktion();
+multiplikation();
+division();
