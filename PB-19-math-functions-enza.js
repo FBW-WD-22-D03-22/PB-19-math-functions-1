824f8bf83fe5eef50ee54e9e6acd2884e3b0e544
Enza <86748625+Enza-von-metaTronics@users.noreply.github.com>
2021-11-23 11:37:45 +0100

    jubel

---
+++ PB-19-math-functions-enza.js
@@ -0,0 +1,36 @@
+const a = 2;
+const b = 3;
+const c = 4;
+
+//Aufgabenteil 1
+
+function addition () { let addition = a + b + c;
+console.log("die summe ist:" , addition);
+}
+
+addition();
+
+//Aufgabenteil 2
+
+function subtraction () { let subtraction = a - b - c;
+    console.log("die summe ist:" , subtraction);
+    }
+    
+    subtraction();
+
+//Aufgabenteil 3
+
+function multiplication () { let multiplication = a * b * c;
+    console.log("die summe ist:" , multiplication);
+    }
+    
+    multiplication();
+
+//Aufgabenteil 4
+
+function division () { let division = a / b / c;
+    console.log("die summe ist:" , division);
+    }
+    
+    division();
+
