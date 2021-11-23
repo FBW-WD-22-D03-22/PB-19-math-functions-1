ec31ced81993d975c200fd5db57d84ffca9250bb
Mohal1125 <rajwindersinghmohal@yahoo.com>
2021-11-23 11:50:19 +0100

    fertig

---
+++ index.js
@@ -0,0 +1,29 @@
+const firstNumber =4;
+const secondNumber =6;
+const thirdNumber =0;
+
+function addition() {
+    let additionSumme = firstNumber + secondNumber + thirdNumber;
+    console.log("addition of const", additionSumme);
+}
+
+function subtraktion() {
+    let subtraktionSumme = firstNumber - secondNumber - thirdNumber;
+    console.log("subtraktion of const", subtraktionSumme);
+}
+
+function multiplikation() {
+    let multiplikationSumme = firstNumber * secondNumber * thirdNumber;
+    console.log("multiplikation of const", multiplikationSumme);
+}
+
+function division() {
+    let divisionSumme = firstNumber / secondNumber / thirdNumber;
+    console.log("division of const", divisionSumme);
+}
+
+addition();
+subtraktion();
+multiplikation();
+division();
+
