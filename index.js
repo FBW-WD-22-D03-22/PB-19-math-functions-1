d4764170cb6e73ce6c3627446b0b3954d8a10f73
Mazen <mazen.taha88@gmail.com>
2021-11-23 12:07:56 +0100

    My lösung

---
+++ index.js
@@ -0,0 +1,32 @@
+const aZahl = 9;
+const bZahl = 6;
+const cZahl = 1;
+
+function addition () {
+    let res = aZahl + bZahl + cZahl;
+    console.log("addition", res)
+}
+
+function Subtraktion () {
+    let res = aZahl - bZahl - cZahl;
+    console.log("Subtraktion", res)
+}
+
+function Multiplikation () {
+    let res = aZahl * bZahl * cZahl;
+    console.log("Multiplikation", res)
+}
+
+function Division () {
+    let res = aZahl / bZahl / cZahl;
+    console.log("Division", res)
+}
+
+
+addition()
+
+Subtraktion()
+
+Multiplikation()
+
+Division()
