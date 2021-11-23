6b09a9976f9eecacdb82d528489b38e9195789c0
jaber-1988 <jaber1988.1988@gmail.com>
2021-11-23 11:46:42 +0100

    jaber

---
+++ index.js
@@ -0,0 +1,29 @@
+const a =5
+const b=4
+const c=9
+
+
+function add(){
+    let res=a+b+c;
+    console.log("res",res);
+}
+
+function sub(){
+    let res=a-b-c;
+    console.log("res",res);
+}
+
+function mul(){
+    let res =a*b*c;
+    console.log("res",res);
+}
+
+function get(){
+    let res=a/b/c;
+    console.log("res",res);
+}
+
+add()
+sub()
+mul()
+get()
