7b35ab6f68e6e5db09891f3ab8f59f7a11004ee5
sprotte <metatron1972@live.de>
2021-11-23 11:48:33 +0100

    sprotte

---
+++ index.js
@@ -0,0 +1,36 @@
+
+
+const ersteZahl = 2;
+const zweiteZahl = 3;
+const driteZahl = 1;
+
+
+function addition() {
+  let resultat = ersteZahl + zweiteZahl + driteZahl;
+  console.log("addition",resultat);
+}
+
+function substraktion() {
+  let substraktion = ersteZahl - zweiteZahl - driteZahl;
+  console.log("Die substraktion ist:",substraktion);
+
+} 
+
+function multiplikation() {
+  let multiplikation = ersteZahl * zweiteZahl * driteZahl;
+  console.log("multiplikation",multiplikation);
+}
+
+function dividieren() {
+  let dividieren = ersteZahl / zweiteZahl / driteZahl;
+  console.log("dividieren",dividieren);
+}
+
+addition();
+substraktion();
+multiplikation();
+dividieren();
+
+
+
+
