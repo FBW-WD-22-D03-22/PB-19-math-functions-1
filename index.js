030d820fa48cb1f881592fa089029d25ff7e990a
kawanaza <kawanbabani@gmail.com>
2021-11-23 12:04:09 +0100

    done

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 
 //let res = a + b + c;
-let a = 10;
-let b = 20 ;
-let c = 30;
-function Summe(){
-console.log("Total",a + b + c);
-}
+const a = 10;
+const b = 20 ;
+const c = 30;
+function Summe () { let Summe = a + b + c;
+    console.log("a + b + c=" , Summe);
+    }
 //let res = a - b - c;
-function minus(){
-    console.log("Total",a - b - c);
+function minus(){ let minus = a - b - c;
+    console.log("a - b - c =" , minus);
     }
 //let res = a * b * c;
-function Multiplikation(){
-    console.log("Total",a * b * c);
+function Multiplikation(){ let Multiplikation = a * b * c;
+    console.log("a * b * c =",Multiplikation);
     }
 //let res = a / b / c;
-function Division(){
-    console.log("Total",a / b / c);
+function Division(){ let Division = a / b / c;
+    console.log("a / b / c=",Division);
     }
 
 
