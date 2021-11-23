52ab0f1f7cd7911219dbaf68b9532d5c4f5035e8
slavablack <veaceslav.ciornii@magenta.de>
2021-11-23 11:49:59 +0100

    Mathematik

---
+++ Mat.js
@@ -0,0 +1,28 @@
+const ersteZahl=45;
+const zweitezahl=48;
+const dritteZahl= 7;
+//+
+function add() {
+    let summe= ersteZahl+zweitezahl+dritteZahl;
+    console.log(summe)
+}
+//-
+function sub() {
+    let summe=ersteZahl-zweitezahl-dritteZahl;
+    console.log(summe)
+}
+//*
+function mult() {
+    let summe=ersteZahl*zweitezahl*dritteZahl;
+    console.log(summe)
+}
+//"/"
+function div() {
+    let summe=ersteZahl/zweitezahl/dritteZahl;
+    console.log(summe)
+}
+
+add();
+sub();
+mult();
+div();
