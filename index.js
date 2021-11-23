10c93f020de601467d02f21e4d8b85b2fa62add1
ShiinyFlakes <mago_elm@hotmail.com>
2021-11-23 11:48:18 +0100

    Commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const a = 5;
+const b = 10;
+const c = 15;
+
+function addition () { let addition = a + b + c;
+console.log("Ergebnis:" , addition);
+}
+
+function subtraktion () { let subtraktion = a - b - c;
+    console.log("Ergebnis:" , subtraktion);
+    }
+    
+
+function multiplikation () { let multiplikation = a * b * c;
+    console.log("Ergebnis:" , multiplikation);
+    }
+    
+
+function division () { let division = a / b / c;
+    console.log("Ergebnis:" , division);
+    }
+    
+    division();
+    addition();
+    subtraktion();
+    multiplikation();
