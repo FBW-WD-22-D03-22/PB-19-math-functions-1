ff647d281c293bf5901f8555b0b2180d269a3800
Dennisbullys <d.rochus@hotmail.de>
2021-11-23 12:07:07 +0100

    Dennis 2 verschieden lösungen

---
+++ Dennis.js
@@ -0,0 +1,38 @@
+console.log("Math-Functions")
+console.log ("---------------")
+
+// Erstelle drei Zahlen als Konstanten.
+ const a = 5;
+ const b = 9;
+ const c = 3;
+
+// Aufgabenteil 1 :
+ let ergebniss = a + b + c
+ console.log (ergebniss )
+
+// Aufgabenteil 2 :
+let res = a - b - c;
+console.log(res )
+
+// Aufgabenteil 3
+let rest = a * b * c;
+console.log(rest)
+
+// Aufgabenteil 4
+let re = a / b / c;
+console.log(re)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Dennis1.js
@@ -0,0 +1,38 @@
+console.log("Math-Functions");
+console.log("---------------");
+
+// Erstelle drei Zahlen als Konstanten.
+const a = 5;
+const b = 9;
+const c = 3;
+
+// Aufgabenteil 1 :
+function addition() {
+  let ergebniss = a + b + c;
+  console.log("addition", ergebniss);
+}
+
+// Aufgabenteil 2 : 
+function substraktion() {
+  let ergebniss = a - b- c;
+  console.log("substraktion", ergebniss);
+}
+
+// Aufgabenteil 3 :
+function multiplikation() {
+  let ergebniss = a * b * c;
+  console.log("multiplikation", ergebniss);
+}
+
+// Aufgabenteil 4 :
+function dividieren() {
+  let ergebniss = a / b / c;
+  console.log("dividieren", ergebniss);
+}
+
+
+
+addition();
+substraktion();
+multiplikation();
+dividieren();
