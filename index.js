398cceb43f021a87cfbf93513399bd863c21869e
kawanaza <kawanbabani@gmail.com>
2021-11-23 11:51:09 +0100

    Das ist mein aufgabe ;)

---
+++ index.js
@@ -0,0 +1,27 @@
+
+//let res = a + b + c;
+let a = 10;
+let b = 20 ;
+let c = 30;
+function Summe(){
+console.log("Total",a + b + c);
+}
+//let res = a - b - c;
+function minus(){
+    console.log("Total",a - b - c);
+    }
+//let res = a * b * c;
+function Multiplikation(){
+    console.log("Total",a * b * c);
+    }
+//let res = a / b / c;
+function Division(){
+    console.log("Total",a / b / c);
+    }
+
+
+
+Summe();
+minus();
+Multiplikation();
+Division();
