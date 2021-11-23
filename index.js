b730ef02898b311c917d85b13409419594fe815e
Hayat Alkheder <hayat.alkheder@gmail.com>
2021-11-23 12:52:02 +0100

    js

---
+++ index.js
@@ -0,0 +1,27 @@
+const firstNumber = 2;
+const secondNumber = 3;
+const thirdNumber = 1;
+
+function addition() {
+    let result1 = firstNumber + secondNumber + thirdNumber;
+    console.log("addition",result1);
+}
+function subtraktion() {
+let result2 = firstNumber - secondNumber - thirdNumber;
+console.log("subtraktion", result2);
+}
+
+function muliplikation(){
+    let result3 = firstNumber * secondNumber * thirdNumber;
+    console.log("muliplikation",result3);
+}
+function division(){
+    let result4 = firstNumber/secondNumber/thirdNumber;
+    console.log("division",result4);
+}
+
+
+addition()
+subtraktion()
+muliplikation()
+division()
