6cf1c70281fb504ebd21cf58b38c159008ae35a9
sham912411 <shamalhakim91@gmail.com>
2021-11-23 11:56:50 +0100

    lösung

---
+++ math.js
@@ -0,0 +1,28 @@
+
+
+//let res = a + b + c;
+let a = 1;
+let b = 2 ;
+let c = 3;
+function Summe(){
+console.log("Total",a + b + c);
+}
+//let res = a - b - c;
+function minus(){
+    console.log("Total",a - b - c);
+    }
+//let res = a * b * c;
+function Multiplikation(){
+    console.log("Total",a * b * c);
+    }
+//let res = a / b / c;
+function Division(){
+    console.log("Total",a / b / c);
+    }
+
+
+
+Summe();
+minus();
+Multiplikation();
+Division();
