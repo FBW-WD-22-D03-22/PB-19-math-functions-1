4de534bd9b1a53d04f2d7bbc4897fcb8727730a4
aisha2000-hub <aisha.noori2000@gmail.com>
2021-11-23 11:58:12 +0100

    Die lösung von Aisha

---
+++ index.js
@@ -0,0 +1,34 @@
+
+const ersteZahl = 2;
+const zweiteZahl = 3;
+const dritteZahl = 4;
+
+// 1)
+function addition(){
+    let result1= ersteZahl + zweiteZahl + dritteZahl;
+    console.log(result1);
+}
+
+// 2)
+function subtraktion(){
+    let result2 = ersteZahl - zweiteZahl - dritteZahl;
+    console.log(result2);
+}
+
+// 3)
+function multiplikation(){
+    let result3 = ersteZahl * zweiteZahl * dritteZahl;
+    console.log(result3);
+}
+
+// 4)
+function division(){
+    let result4 = ersteZahl / zweiteZahl / dritteZahl;
+    console.log(result4);
+
+}
+
+addition()
+subtraktion()
+multiplikation()
+division()
