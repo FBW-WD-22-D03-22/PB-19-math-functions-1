76cd24ba1968043cdfc6efbaa4f391f1978cb276
Vens <v.kolev@alfatrex.com>
2021-11-23 11:55:31 +0100

    vens

---
+++ vens.js
@@ -0,0 +1,40 @@
+// Aufgabenteil 1
+let a = 3;
+let b = 7;
+let c = 9;
+
+//Aufgabenteil 1
+let res = a + b + c;
+
+function addieren() {
+    console.log(res);
+}
+
+addieren();
+
+//Aufgabenteil 2
+res = a - b - c;
+
+function subtraktion() {
+    console.log(res);
+}
+
+subtraktion();
+
+//Aufgabenteil 3
+res = a * b * c;
+
+function multiplikation() {
+    console.log(res);
+}
+
+multiplikation();
+
+//Aufgabenteil 4
+res = a / b / c;
+
+function division() {
+    console.log(res);
+}
+
+division();
